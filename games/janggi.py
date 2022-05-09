fffce5073f281daceb193d8364fd6ba334f2d47e
Sungho Cho <sunghoch@andrew.cmu.edu>
2022-05-09 16:36:01 -0600

    Allow max rounds to be 200

--- games/janggi.py
+++ games/janggi.py
@@ -36,7 +36,7 @@ class MuZeroConfig:
         # Self-Play
         self.num_workers = 10  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
-        self.max_moves = 121  # Maximum number of moves if game is not finished before
+        self.max_moves = 200  # Maximum number of moves if game is not finished before
         self.num_simulations = 400  # Number of future moves self-simulated
         self.discount = 1  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
@@ -189,7 +189,9 @@ class Game(AbstractGame):
         Returns:
             Initial observation of the game.
         """
-        return self.get_observation(self.env.reset())
+        observation = self.env.reset()
+        self.turn = self.env.turn
+        return self.get_observation(observation)
 
     def close(self):
         """
@@ -226,117 +228,3 @@ class Game(AbstractGame):
             String representing the action.
         """
         return self.env.action_to_human_input(action)
-
-
-class Janggi:
-    def __init__(self):
-        self.board_size = 11
-        self.board = numpy.zeros(
-            (self.board_size, self.board_size), dtype="int32")
-        self.player = 1
-        self.board_markers = [
-            chr(x) for x in range(ord("A"), ord("A") + self.board_size)
-        ]
-
-    def to_play(self):
-        return 0 if self.player == 1 else 1
-
-    def reset(self):
-        self.board = numpy.zeros(
-            (self.board_size, self.board_size), dtype="int32")
-        self.player = 1
-        return self.get_observation()
-
-    def step(self, action):
-        x = math.floor(action / self.board_size)
-        y = action % self.board_size
-        self.board[x][y] = self.player
-
-        done = self.is_finished()
-
-        reward = 1 if done else 0
-
-        self.player *= -1
-
-        return self.get_observation(), reward, done
-
-    def get_observation(self):
-        board_player1 = numpy.where(self.board == 1, 1.0, 0.0)
-        board_player2 = numpy.where(self.board == -1, 1.0, 0.0)
-        board_to_play = numpy.full((11, 11), self.player, dtype="int32")
-        return numpy.array([board_player1, board_player2, board_to_play])
-
-    def legal_actions(self):
-        legal = []
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                if self.board[i][j] == 0:
-                    legal.append(i * self.board_size + j)
-        return legal
-
-    def is_finished(self):
-        has_legal_actions = False
-        directions = ((1, -1), (1, 0), (1, 1), (0, 1))
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                # if no stone is on the position, don't need to consider this position
-                if self.board[i][j] == 0:
-                    has_legal_actions = True
-                    continue
-                # value-value at a coord, i-row, j-col
-                player = self.board[i][j]
-                # check if there exist 5 in a line
-                for d in directions:
-                    x, y = i, j
-                    count = 0
-                    for _ in range(5):
-                        if (x not in range(self.board_size)) or (
-                            y not in range(self.board_size)
-                        ):
-                            break
-                        if self.board[x][y] != player:
-                            break
-                        x += d[0]
-                        y += d[1]
-                        count += 1
-                        # if 5 in a line, store positions of all stones, return value
-                        if count == 5:
-                            return True
-        return not has_legal_actions
-
-    def render(self):
-        marker = "  "
-        for i in range(self.board_size):
-            marker = marker + self.board_markers[i] + " "
-        print(marker)
-        for row in range(self.board_size):
-            print(chr(ord("A") + row), end=" ")
-            for col in range(self.board_size):
-                ch = self.board[row][col]
-                if ch == 0:
-                    print(".", end=" ")
-                elif ch == 1:
-                    print("X", end=" ")
-                elif ch == -1:
-                    print("O", end=" ")
-            print()
-
-    def human_input_to_action(self):
-        human_input = input("Enter an action: ")
-        if (
-            len(human_input) == 2
-            and human_input[0] in self.board_markers
-            and human_input[1] in self.board_markers
-        ):
-            x = ord(human_input[0]) - 65
-            y = ord(human_input[1]) - 65
-            if self.board[x][y] == 0:
-                return True, x * self.board_size + y
-        return False, -1
-
-    def action_to_human_input(self, action):
-        x = math.floor(action / self.board_size)
-        y = action % self.board_size
-        x = chr(x + 65)
-        y = chr(y + 65)
-        return x + y
