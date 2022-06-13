7803da136826074bd7522e83112e6d4f95119776
Sungho Cho <didog9595@gmail.com>
2022-06-14 03:10:34 +0900

    Reduce number of stacked observations from 100 to 8

--- games/janggi.py
+++ games/janggi.py
@@ -26,7 +26,7 @@ class MuZeroConfig:
         self.observation_shape = (15, 10, 9)  # Dimensions of the game observation, must be 3 (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
         self.action_space = list(range(9999))  # Fixed list of all possible actions. You should only edit the length
         self.players = list(range(2))  # List of players. You should only edit the length
-        self.stacked_observations = 100  # Number of previous observations and previous actions to add to the current observation
+        self.stacked_observations = 8  # Number of previous observations and previous actions to add to the current observation
 
         # Evaluate
         self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
